ce288b6a347a75d0c12924778094c8f3324e87a4
Arnab Jyoti Borah <113490566+Arnab7747@users.noreply.github.com>
2024-11-07 23:16:59 +0530

    signupPage

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,21 +1,27 @@
-import { Outlet,Navigate } from "react-router-dom"
+import { Outlet, Navigate } from "react-router-dom";
 
-function AuthLayout() {
-  const isAutheticate=false;
+
+
+export default function AuthLayout() {
+  const isAuthenticated  = false;
+
   return (
-    <div>
-      {isAutheticate?(
-        <Navigate to="/"/>
-      ):(
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
         <>
-        <section>
-          <Outlet/>
-        </section>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
+
+          <img
+            src="/assets/images/sideImage.jpg"
+            alt="logo"
+            className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+          />
         </>
       )}
-      
-    </div>
-  )
+    </>
+  );
 }
-
-export default AuthLayout
